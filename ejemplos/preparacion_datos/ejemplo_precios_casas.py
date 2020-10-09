f46e7762541c10a3ba1d974bebbeefd8b4fa3fbd
Aurea Soriano-Vargas <aurea.soriano@gmail.com>
2020-10-08 21:32:34 -0300

    ejemplo de precios de casas finalizado

--- ejemplos/preparacion_datos/ejemplo_precios_casas.py
+++ ejemplos/preparacion_datos/ejemplo_precios_casas.py
@@ -31,8 +31,37 @@ df = pd.read_csv("../../datasets/USA_Housing.csv")
 #df['Price'].plot.density()
 #plt.show()
 
-print(df.corr())
+#print(df.corr())
 
-plt.figure(figsize=(10,7))
-sns.heatmap(df.corr(), linewidths=2)
-plt.show()
+#plt.figure(figsize=(10,7))
+#sns.heatmap(df.corr(), linewidths=2)
+#plt.show()
+
+
+#clasificacion
+#cuales son las variables independientes que vamos a usar
+#cual es la variable dependiente que vamos a categorizar o una variable que de por si
+#sea categorizado
+# caras #moderadas #economicas
+# precio -> categorias  X> m1 (caro) m2<X<m3(moderado) X<m4(economico)
+
+
+#Regresion -> estimar el precio cuanto costaria esa casa, considerando todas las
+#caracteristicas dadas.
+# todas las variables - direccion  -> precio
+
+print(df.columns)
+
+column_size = len(list(df.columns))
+print(column_size)
+
+# matriz nuestras variables numericas excepto el precio
+X = df[df.columns[0:column_size-2]]
+#lista para ser usada en los sgtes Parsonschester
+
+# donde vamos a tener la variable objetivo
+y = df[df.columns[column_size-2]]
+#print(y)
+
+
+#70% - 30% / 80%- 20%
