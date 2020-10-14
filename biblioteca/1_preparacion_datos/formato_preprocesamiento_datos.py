c745b6589f67f0fd5f7ac5a9a40b608f6cc34126
Aurea Soriano-Vargas <aurea.soriano@gmail.com>
2020-10-13 22:14:06 -0300

    biblioteca preparacion (1)

--- biblioteca/1_preparacion_datos/formato_preprocesamiento_datos.py
+++ biblioteca/1_preparacion_datos/formato_preprocesamiento_datos.py
@@ -5,125 +5,169 @@
 .. autora:: Dra. Aurea Soriano <aurea.soriano@ic.unicamp.br>
 
 """
-# Importando bibliotecas
+
+#Importar las bibliotecas
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+
+#biblioteca para interpolar datos faltantes
+from sklearn.impute import SimpleImputer
+
+# Para dividir nuestro conjunto de Datos
+from sklearn.model_selection import train_test_split
 
 
 
-# Importando el conjunto de datos
+#Leer nuestro conjunto de Datos
 dataset = pd.read_csv('ejemplo_datos.csv')
+
+
 
 X = dataset.iloc[:, [0,1,2]].values
 y = dataset.iloc[:, 3].values
 
-# Conociendo nuestros Datos
-print("*** conciendo nuestros datos***")
-print("cabecera")
-print(dataset.head())
-print("descriptor de caracteristicas")
-print(dataset.describe())
-print("informacion sobre nulos y el tipo de objetos que hay en las columnas")
-print(dataset.info())
+#print("Variables a ser usadas en la prediccion")
+#print(X)
 
-# Verificando si hay datos faltantes
-print("*** verificando si hay datos faltantes")
-print(dataset.isnull().values.any())
+#print("Variable objetivo")
+#print(y)
 
-# Verificando si hay nans
-print("*** verificando si hay NaN (not a number)")
-print(dataset.isnull().sum().sum())
+#Conocer nuestros Datos
+#print("cabecera")
+#print(dataset.head())
 
-# Estrategia para datos faltantes
+
+#print("descripcion de los datos")
+#print(dataset.describe())
+
+
+#print("verificar si hay datos nulos y que tipo de datos son")
+#print(dataset.info())
+
+
+
+#verificar si hay nulos (nan)
+#print("verificar si hay NaN(not a number)")
+#print(dataset.isnull().sum().sum())
+
+
+
+#Estrategia para datos faltantes
+
+
 #1era estrategia: reemplazar con 0 cada valor NaN de cada columna
 
-#forma 1
+#forma1
 dataset2 = dataset.copy()
 dataset2['Age'] = dataset2['Age'].fillna(0)
 dataset2['Salary'] = dataset2['Salary'].fillna(0)
 
-#forma 2
+#print(dataset2)
+
+
+#forma2
 dataset3 = dataset.copy()
-dataset3['Age'] = dataset2['Age'].replace(np.nan, 0)
-dataset3['Salary'] = dataset2['Salary'].replace(np.nan, 0)
+dataset3['Age'] = dataset3['Age'].replace(np.nan, 1000)
+dataset3['Salary'] = dataset3['Salary'].replace(np.nan, 1000)
 
 
-#2da estrategia:reemplazar todas los valores NaN de todas las columnas
+#print(dataset3)
 
-#forma 1
+
+
+#2da estrategia: reemplazar todos los valores NaN de todas las columnas
 dataset4 = dataset.copy()
-dataset4 =dataset4.fillna(0)
+dataset4 = dataset4.fillna(0)
+#print(dataset4)
 
 
-#forma 2
 dataset5 = dataset.copy()
-dataset5 = dataset5.replace(np.nan,0)
+dataset5 = dataset5.replace(np.nan, 1000)
+#print(dataset5)
 
 
-#3era estrategia: NaN usando la media de los vecinos
-
-# biblioteca que nos hara ese calculo
-from sklearn.impute import SimpleImputer
+#3era estrategia: reemplazar los valores con estadisticas de los vecinos
 
 
-# obtenemos los valores
 dataset6 = dataset.copy()
 
-# creamos el imputer que es el que nos hara la magia
-#otras estrategias que acepta imputer "mean", “median”, “most_frequent”,
-#“constant” (pasandole una constante)
-imr = SimpleImputer(missing_values=np.nan, strategy='mean')
+# a crear nuestro objeto SimpleImputer
+# strategies: mean, median, most_frequent, constant(una constante definida por
+#el usuario)
+
+imr = SimpleImputer(missing_values=np.nan, strategy='median')
 imr = imr.fit(dataset6[['Age']])
+
+#print(imr.transform(dataset6[['Age']]))
+#print(imr.transform(dataset6[['Age']]).ravel())
 dataset6['Age'] = imr.transform(dataset6[['Age']]).ravel()
+#print(dataset6['Age'])
+
+
 imr = imr.fit(dataset6[['Salary']])
 dataset6['Salary'] = imr.transform(dataset6[['Salary']]).ravel()
+#print(dataset6['Salary'])
 
 
-
-# Datos Categoricos
-#Country y Purchased
+# Limpiar, transformar nuestros datos categoricos
+#Country y Purchased como texto (categoricos)
 
 
 #1era estrategia: get dummies
+dataset7 =  dataset6.copy()
+dataset7 = pd.get_dummies(dataset7, columns=["Country"])
+print(dataset7)
+dataset7["Purchased_number"] = np.zeros(len(dataset7))
 
-dataset7 = dataset6.copy()
-dataset7= pd.get_dummies(dataset7)
+for i in range(0, len(dataset7)):
+    if dataset7["Purchased"][i] ==  "Yes":
+        dataset7["Purchased_number"][i] = 1
+    else:
+        dataset7["Purchased_number"][i] = 0
 
-#2da estrategia: manual
+del dataset7["Purchased"]
+
+print(dataset7)
+
+
+#2da estrategia: hacerlo manualmente
+dataset8 = dataset6.copy()
+
 countries = ["France", "Spain", "Germany"]
 purchased = ["No", "Yes"]
 
-dataset8 = dataset6.copy()
 
 for i in range(0, len(dataset8)):
     dataset8["Country"][i] = countries.index(dataset8["Country"][i])
     dataset8["Purchased"][i] = purchased.index(dataset8["Purchased"][i])
 
 
-#3era estrategia: remover esas columnas
+#print(dataset8)
+
+
+#3era estrategia: remover las columnas categoricas
 
 dataset9 = dataset6.copy()
-#del dataset9['Country']
-#del dataset9['Purchased']
-dataset9 = dataset9.drop(['Country', 'Purchased'], axis = 1)
+dataset9 = dataset9.drop(['Country', 'Purchased'], axis=1)
+#print(dataset9)
 
 
+#Ya tenemos nuestro conjunto limpio (dataset7)
 
 
-# Dividir el conjunto de datos en entrenamiento y prueba
-print("dividiendo el conjunto de entrenamiento y prueba")
-from sklearn.model_selection import train_test_split
-
-l_column = list(dataset8.columns) # Making a list out of column names
-len_feature = len(l_column) # Length of column vector list
+#aprendizaje no supervisado
+X = dataset7.iloc[:, [0,1,2,3,4,5]].values
 
 
-#usaremos el dataset 8
-X = dataset8[l_column[0:len_feature-1]]
-y = dataset8[l_column[len_feature-1]]
+#aprendizaje supervisado
+# las variables que van a ser usadas en la prediccion (Regresion o clasificacion)
+X = dataset7.iloc[:, [0,1,2,3,4]].values
+#la variable a ser predicha
+y = dataset7.iloc[:, 5].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-print(X_train)
-print(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print(len(X_train))
+print(len(X_test))
