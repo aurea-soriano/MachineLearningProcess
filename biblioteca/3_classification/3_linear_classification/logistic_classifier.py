9517df7c8a555f79bc66f3844141fb6102351c6c
Aurea Soriano-Vargas <aurea.soriano@gmail.com>
2020-10-22 22:48:02 -0300

    logistic regression clas.

---
+++ biblioteca/3_classification/3_linear_classification/logistic_classifier.py
@@ -0,0 +1,70 @@
+"""
+.. biblioteca:: Classification with Logistic Regression.
+   :plataforma: Unix, Windows, MAC
+   :sinopsis: Classification
+.. autora:: Dra. Aurea Soriano <aurea.soriano@ic.unicamp.br>
+"""
+'''
+Conseguir una version binaria. Como? Vamos a asignar la clase 0
+si el valor de salida es menor igual a 0.5 y vamos a asignar la
+clase 1 cuando el valor de salida es mayor a 0.5
+
+'''
+
+#importando nuestras librerias
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+#matriz de confusion
+from sklearn.metrics import confusion_matrix
+
+
+
+#importar el dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+#print(dataset.describe())
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:, 4].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+classifier =  LogisticRegression()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+
+cm  = confusion_matrix(y_test, y_pred)
+print(cm)
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression Linear (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
